d1a0edf2c84c02ef5c9cb4728480ddd9fbb4dae0
jounsy <jouns@mail.ru>
2020-05-27 19:45:57 +0300

    Add methods for buttons increase/decrease quantity

--- app/src/main/java/com/snowman/musicshop/MainActivity.java
+++ app/src/main/java/com/snowman/musicshop/MainActivity.java
@@ -3,12 +3,31 @@ package com.snowman.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int countItems = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void increaseQuantity(View view) {
+        TextView quantity = findViewById(R.id.textItemsCount);
+        quantity.setText(String.valueOf(++countItems));
+
+    }
+
+    public void decreaseQuantity(View view) {
+        TextView quantity = findViewById(R.id.textItemsCount);
+        if (countItems > 0) {
+            quantity.setText(String.valueOf(--countItems));
+        }
+        else{quantity.setText("0");
+        }
+    }
+    
 }
