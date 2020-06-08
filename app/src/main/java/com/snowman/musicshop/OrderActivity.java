dd60049daa88a4514f3e63d3fe53740766fede03
jounsy <jouns@mail.ru>
2020-06-08 10:59:49 +0300

    Add email sending

--- app/src/main/java/com/snowman/musicshop/OrderActivity.java
+++ app/src/main/java/com/snowman/musicshop/OrderActivity.java
@@ -3,10 +3,16 @@ package com.snowman.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    String addresses[] = {"Ndrdtstwerty@gmail.com"};
+    String subject = "Order from Music Shop"; //Тема
+    String emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +24,21 @@ public class OrderActivity extends AppCompatActivity {
         String goodsName = recievedIntent.getStringExtra("goodsName");
         int quantity = recievedIntent.getIntExtra("countItems",0);
         double orderPrice = recievedIntent.getDoubleExtra("price",0.0);
+        emailText = "Customer Name: " + userName + "\n Goods name: " + goodsName + "\n Quantity: " + quantity +"\n Price: " + orderPrice + "\n Order price: " + orderPrice * quantity;
         TextView textView = findViewById(R.id.orderTextView);
-        textView.setText("Customer Name: " + userName + "\n Goods name: " + goodsName + "\n Quantity: " + quantity +"\n Price: " + orderPrice + "\n Order price: " + orderPrice * quantity);
+        textView.setText(emailText);
         setTitle(R.string.order_activity_name);
     }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
